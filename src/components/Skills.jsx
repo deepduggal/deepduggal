5f42002a0c72d38d0e9e6bba927b8c03232f9a58
Deep Duggal <7544238+deepduggal@users.noreply.github.com>
2023-11-30 13:15:38 -0500

    Moved skills to src/data

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { ChipSet, Chip } from '@rmwc/chip';
-
-const skillsData = ['React', 'TypeScript', 'JavaScript', 'HTML', 'CSS', 'Expo', 'React Native', 'Express JS', 'MongoDB', 'Mongoose JS', 'Jest', 'React Native Testing Library'];
+import skills from '../data/skills';
 
 function Skills() {
   return (
     <ChipSet>
-      {skillsData.map(label => <Chip key={label} label={label}></Chip>)}
+      {skills.map(label => <Chip key={label} label={label}></Chip>)}
     </ChipSet>
   );
 }
---
+++ src/data/skills.js
@@ -0,0 +1,3 @@
+const skills = ['React', 'TypeScript', 'JavaScript', 'HTML', 'CSS', 'Expo', 'React Native', 'Express JS', 'MongoDB', 'Mongoose JS', 'Jest', 'React Native Testing Library'];
+
+export default skills;
